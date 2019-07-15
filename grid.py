3cb36d080defc48a794b0f9f2408932d2f2a0723
Connor Luebbehusen <connorluebbehusen@hotmail.com>
2019-07-15 07:47:50 -0500

    added recursive solver, fixed minor issues with grid class, and fixed issue with how empty cells are found

--- grid.py
+++ grid.py
@@ -1,39 +1,53 @@
 class Cell:
 
     def __init__(self, x, y, value):
-        self.x = x
-        self.y = y
-        self.value = value
-        if x in range(0,2):
-            if y in range(0,2):
+        self.x = int(x)
+        self.y = int(y)
+        self.value = int(value)
+        if x in range(0,3):
+            if y in range(0,3):
                 self.region = 0
-            elif y in range(3,5):
+            elif y in range(3,6):
                 self.region = 1
-            elif y in range(6,8):
+            elif y in range(6,9):
                 self.region = 2
-        elif x in range(3,5):
-            if y in range(0,2):
+        elif x in range(3,6):
+            if y in range(0,3):
                 self.region = 3
-            elif y in range(3,5):
+            elif y in range(3,6):
                 self.region = 4
-            elif y in range(6,8):
+            elif y in range(6,9):
                 self.region = 5
-        elif x in range(6,8):
-            if y in range(0,2):
+        elif x in range(6,9):
+            if y in range(0,3):
                 self.region = 6
-            elif y in range(3,5):
+            elif y in range(3,6):
                 self.region = 7
-            elif y in range(6,8):
+            elif y in range(6,9):
                 self.region = 8
 
     def __str__(self):
+        if self.value == 0:
+            return '-'
         return str(self.value)
 
 
 class Grid:
 
-    def __init__(self):
-        self.grid = [[Cell(x, y, 0) for x in range(9)] for y in range(9)]
+    def __init__(self, file):
+        self.grid = []
+        x = 0
+        with open (file, 'r') as grid_file:
+            for line in grid_file:
+                y = 0
+                cell_list = []
+                line = line.strip()
+                value_list = list(line.split(' '))
+                for value in value_list:
+                    cell_list.append(Cell(x, y, value))
+                    y += 1
+                self.grid.append(cell_list)
+                x += 1
 
     def __str__(self):
         return_string = ''
@@ -46,43 +60,48 @@ class Grid:
         return_string = return_string[0:-1]
         return return_string
 
-    def change_cell(self, x, y, new_value):
-        try:
-            self.grid[x][y].value = new_value
-        except Exception as e:
-            print(e)
-            print('Attempted to access invalid index.')
-
     def get_empty_cell(self):
+        x = 0
+        y = 0
         for row in self.grid:
             for cell in row:
-                if cell == 0:
-                    return (cell.x, cell.y)
+                if y > x and self.grid[x][y].value == 0:
+                    return self.grid[x][y]
+                if cell.value == 0:
+                    return cell
+                y += 1
+            y = 0
+            x += 1
         return False
 
-    def check_row_validity(self, target_cell):
-        for cell in self.grid[x]:
-            if cell == target_cell.value:
+    def check_row_validity(self, target_cell, test_value):
+        for cell in self.grid[target_cell.x]:
+            if cell.x == target_cell.x and cell.y == target_cell.y:
+                pass
+            elif cell.value == test_value:
                 return False
         return True
 
-    def check_col_validity(self, target_cell):
+    def check_col_validity(self, target_cell, test_value):
         for row in self.grid:
-            if row[y] == target_cell.value:
+            cell = row[target_cell.y]
+            if cell.x == target_cell.x and cell.y == target_cell.y:
+                pass
+            elif row[target_cell.y].value == test_value:
                 return False
         return True
 
-    def check_region_validity(self, target_cell):
+    def check_region_validity(self, target_cell, test_value):
         for row in self.grid:
             for cell in row:
                 if cell.x == target_cell.x and cell.y == target_cell.y:
                     pass
-                elif (cell.region == target_cell.region) and
-                   (cell.value == target_cell.value):
+                elif ((cell.region == target_cell.region) and
+                     (cell.value == test_value)):
                    return False
         return True
 
-    def check_validity(self, target_cell):
-        return (check_row_validity(target_cell) and
-                check_col_validity(target_cell) and
-                check_region_validity(target_cell))
+    def check_validity(self, target_cell, test_value):
+        return (self.check_row_validity(target_cell, test_value) and
+                self.check_col_validity(target_cell, test_value) and
+                self.check_region_validity(target_cell, test_value))
---
+++ solver.py
@@ -0,0 +1,16 @@
+import os
+clear = lambda: os.system('clear')
+
+def backtracking_solve(grid):
+    cell = grid.get_empty_cell()
+    if not cell:
+        print('== Puzzle Solved ==')
+        print(grid)
+        return True
+    for value in range(1, 10):
+        if grid.check_validity(cell, value):
+            cell.value = value
+            if backtracking_solve(grid):
+                return True
+            cell.value = 0
+    return False
